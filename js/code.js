9d9d22a6ae4fff336b9d150781e38ccd81b5deea
Kamil Kalbarczyk <k.kalbarczyk1@gmail.com>
2021-12-28 13:33:15 +0100

    fix overflow in edit record

--- js/code.js
+++ js/code.js
@@ -1,151 +1,176 @@
 // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyBYTfRxehiM77l29mnWhxeydOUx5Npyjqg",
-    authDomain: "baby-feeding-kk.firebaseapp.com",
-    projectId: "baby-feeding-kk",
-    storageBucket: "baby-feeding-kk.appspot.com",
-    messagingSenderId: "25079389571",
-    appId: "1:25079389571:web:a8cdd0e81999ed815deab6"
-  };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYTfRxehiM77l29mnWhxeydOUx5Npyjqg",
+  authDomain: "baby-feeding-kk.firebaseapp.com",
+  projectId: "baby-feeding-kk",
+  storageBucket: "baby-feeding-kk.appspot.com",
+  messagingSenderId: "25079389571",
+  appId: "1:25079389571:web:a8cdd0e81999ed815deab6",
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-  import { getFirestore, doc, setDoc, collection, addDoc, getDocs, query, orderBy, limit, updateDoc, getDoc   } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js'
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  collection,
+  addDoc,
+  getDocs,
+  query,
+  orderBy,
+  limit,
+  updateDoc,
+  getDoc,
+} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 
-  const db = getFirestore();
-  const q = query(collection(db, "feeds"), orderBy("date", "desc"), limit(4));
-
+const db = getFirestore();
+const q = query(collection(db, "feeds"), orderBy("date", "desc"), limit(4));
 
 // Variables
 // const breasts = document.querySelector('main');
-const leftBreastButton = document.querySelector('main div.leftBreast');
-const rightBreastButton = document.querySelector('main div.rightBreast');
-const hisotryUl = document.querySelector('.history__list');
+const leftBreastButton = document.querySelector("main div.leftBreast");
+const rightBreastButton = document.querySelector("main div.rightBreast");
+const hisotryUl = document.querySelector(".history__list");
 let hisotryList;
-const dateInput = document.querySelector('input[type = date');
-const timeInput = document.querySelector('input[type = time');
+const dateInput = document.querySelector("input[type = date");
+const timeInput = document.querySelector("input[type = time");
 let hisotryRecordsDivs;
 // let feedingHistory = [];
 
 // add current time to the inputs
 
 // dateInput.valueAsDate = new Date();
-dateInput.value = `${new Date().getFullYear()}-${new Date().getMonth()+1}-${new Date().getDate()}`
-dateInput.setAttribute('max', `${new Date().getFullYear()}-${new Date().getMonth()+1}-${new Date().getDate()}`);
+dateInput.value = `${new Date().getFullYear()}-${
+  new Date().getMonth() + 1
+}-${new Date().getDate()}`;
+dateInput.setAttribute(
+  "max",
+  `${new Date().getFullYear()}-${
+    new Date().getMonth() + 1
+  }-${new Date().getDate()}`
+);
 
-timeInput.value = new Date().toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'});
-
+timeInput.value = new Date().toLocaleTimeString(navigator.language, {
+  hour: "2-digit",
+  minute: "2-digit",
+});
 
 // add data to Firestore
-      // Add a new document with a generated id.
-      const addDataToFirestore = async (breast, date) => {
-        const docRef = await addDoc(collection(db, "feeds"), {
-        breast: breast,
-        date: date
-            });
-        // console.log("Document written with ID: ", docRef.id);
-      };
+// Add a new document with a generated id.
+const addDataToFirestore = async (breast, date) => {
+  const docRef = await addDoc(collection(db, "feeds"), {
+    breast: breast,
+    date: date,
+  });
+  // console.log("Document written with ID: ", docRef.id);
+};
 
-      const changeBreast = async (breast ) => {
-        const docRef = doc(db, "nextBreast", "breast");
-         await updateDoc(docRef, {
-                    nextBreast: breast,
-                });
-        // console.log("Document written with ID: ", docRef.id);
-      }
+const changeBreast = async (breast) => {
+  const docRef = doc(db, "nextBreast", "breast");
+  await updateDoc(docRef, {
+    nextBreast: breast,
+  });
+  // console.log("Document written with ID: ", docRef.id);
+};
 
-      const addDataToFirebaseWithDateAsDoc = async (breast, date) => {
-        // const docRef = await addDoc(collection(db, dateInput.value), {
-        // breast: breast,
-        // date: date
-        //     });
+const addDataToFirebaseWithDateAsDoc = async (breast, date) => {
+  // const docRef = await addDoc(collection(db, dateInput.value), {
+  // breast: breast,
+  // date: date
+  //     });
 
-            await setDoc(doc(db, dateInput.value, timeInput.value), {
-            breast: breast,
-            date: date
-            });
-        // console.log("Document written with ID: ", docRef.id);
-      };
+  await setDoc(doc(db, dateInput.value, timeInput.value), {
+    breast: breast,
+    date: date,
+  });
+  // console.log("Document written with ID: ", docRef.id);
+};
 
 // chose breast functions
 
 function choseLeftBreast(e) {
-    leftBreastButton.classList.remove('nextBreast');
-    leftBreastButton.classList.add('lastBreast');
-    rightBreastButton.classList.add('nextBreast');
-    rightBreastButton.classList.remove('lastBreast');
-    // lastLeftBreast = true;
-    // const newFeeding = {
-    //     breast: 'lewa',
-    //     date: `${dateInput.value} ${timeInput.value}`
-    // };
-    // feedingHistory.push(newFeeding);
-    addDataToFirestore('lewa', `${dateInput.value} ${timeInput.value}`);
-    changeBreast('prawa');
-    renderFeeds();
-    addDataToFirebaseWithDateAsDoc('lewa', `${dateInput.value} ${timeInput.value}`);
-};
-
-function choseRightBreast(e) {
-    rightBreastButton.classList.remove('nextBreast');
-    rightBreastButton.classList.add('lastBreast');
-    leftBreastButton.classList.add('nextBreast');
-    leftBreastButton.classList.remove('lastBreast');
-    // lastLeftBreast = false;
-   const newFeeding = {
-        breast: 'prawa',
-        date: `${dateInput.value} ${timeInput.value}`
-    };
-    // feedingHistory.push(newFeeding);
-    addDataToFirestore('prawa', `${dateInput.value} ${timeInput.value}`);
-     changeBreast('lewa');
-    renderFeeds();
-    addDataToFirebaseWithDateAsDoc('prawa', `${dateInput.value} ${timeInput.value}`);
+  leftBreastButton.classList.remove("nextBreast");
+  leftBreastButton.classList.add("lastBreast");
+  rightBreastButton.classList.add("nextBreast");
+  rightBreastButton.classList.remove("lastBreast");
+  // lastLeftBreast = true;
+  // const newFeeding = {
+  //     breast: 'lewa',
+  //     date: `${dateInput.value} ${timeInput.value}`
+  // };
+  // feedingHistory.push(newFeeding);
+  addDataToFirestore("lewa", `${dateInput.value} ${timeInput.value}`);
+  changeBreast("prawa");
+  renderFeeds();
+  addDataToFirebaseWithDateAsDoc(
+    "lewa",
+    `${dateInput.value} ${timeInput.value}`
+  );
 }
 
+function choseRightBreast(e) {
+  rightBreastButton.classList.remove("nextBreast");
+  rightBreastButton.classList.add("lastBreast");
+  leftBreastButton.classList.add("nextBreast");
+  leftBreastButton.classList.remove("lastBreast");
+  // lastLeftBreast = false;
+  const newFeeding = {
+    breast: "prawa",
+    date: `${dateInput.value} ${timeInput.value}`,
+  };
+  // feedingHistory.push(newFeeding);
+  addDataToFirestore("prawa", `${dateInput.value} ${timeInput.value}`);
+  changeBreast("lewa");
+  renderFeeds();
+  addDataToFirebaseWithDateAsDoc(
+    "prawa",
+    `${dateInput.value} ${timeInput.value}`
+  );
+}
 
 // render feeds
 
 const renderFeeds = async () => {
-    hisotryUl.innerHTML= '';
+  hisotryUl.innerHTML = "";
 
-      const querySnapshot = await getDocs(q);
-        querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        //  console.log(doc.id, " => ", doc.data());
-          const li = document.createElement('li');
-            const breast = document.createElement('p');
-            breast.textContent = doc.data().breast;
-            const date = document.createElement('p');
-            date.textContent = doc.data().date;
-            const div = document.createElement('div');
-            div.appendChild(breast);
-            div.appendChild(date);
-            li.appendChild(div);
-            // li.innerHTML = `
-            //  <div>
-            // <p class="history__breast">${doc.data().breast}</p>
-            // <p class="history__date">${doc.data().date}</p>
-            //  </div>`;
-            hisotryUl.appendChild(li);
-            if (doc.data().breast === 'lewa') {
-               div.classList.add('leftBreast');
-            } else {
-                div.classList.add('rightBreast');
-            }
-        });
-hisotryList = document.querySelectorAll('.history__list li');
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    //  console.log(doc.id, " => ", doc.data());
+    const li = document.createElement("li");
+    const breast = document.createElement("p");
+    breast.textContent = doc.data().breast;
+    const date = document.createElement("p");
+    date.textContent = doc.data().date;
+    const div = document.createElement("div");
+    div.appendChild(breast);
+    div.appendChild(date);
+    li.appendChild(div);
+    // li.innerHTML = `
+    //  <div>
+    // <p class="history__breast">${doc.data().breast}</p>
+    // <p class="history__date">${doc.data().date}</p>
+    //  </div>`;
+    hisotryUl.appendChild(li);
+    if (doc.data().breast === "lewa") {
+      div.classList.add("leftBreast");
+    } else {
+      div.classList.add("rightBreast");
+    }
+  });
+  hisotryList = document.querySelectorAll(".history__list li");
 
-hisotryRecordsDivs = document.querySelectorAll('.history__list div');
-hisotryRecordsDivs.forEach((record) => {
-  record.addEventListener('click', editRecord);
-})
+  hisotryRecordsDivs = document.querySelectorAll(".history__list div");
+  hisotryRecordsDivs.forEach((record) => {
+    record.addEventListener("click", editRecord);
+  });
 };
 
 renderFeeds();
@@ -153,34 +178,33 @@ renderFeeds();
 // check last breast use
 
 const checkLastBreast = async () => {
-    const docRef = doc(db, "nextBreast", "breast");
-    const docSnap = await getDoc(docRef);
-    
-if (docSnap.data().nextBreast === 'lewa') {
-  leftBreastButton.classList.add('nextBreast');
-  rightBreastButton.classList.remove('nextBreast');
-      leftBreastButton.classList.remove('lastBreast');
-  rightBreastButton.classList.add('lastBreast');
+  const docRef = doc(db, "nextBreast", "breast");
+  const docSnap = await getDoc(docRef);
 
-  console.log("Document data:", docSnap.data());
-} else {
-  leftBreastButton.classList.remove('nextBreast');
-  rightBreastButton.classList.add('nextBreast');
-      leftBreastButton.classList.add('lastBreast');
-  rightBreastButton.classList.remove('lastBreast');
-}
+  if (docSnap.data().nextBreast === "lewa") {
+    leftBreastButton.classList.add("nextBreast");
+    rightBreastButton.classList.remove("nextBreast");
+    leftBreastButton.classList.remove("lastBreast");
+    rightBreastButton.classList.add("lastBreast");
+
+    console.log("Document data:", docSnap.data());
+  } else {
+    leftBreastButton.classList.remove("nextBreast");
+    rightBreastButton.classList.add("nextBreast");
+    leftBreastButton.classList.add("lastBreast");
+    rightBreastButton.classList.remove("lastBreast");
+  }
 };
 checkLastBreast();
 
-checkLastBreast()
+checkLastBreast();
 
-leftBreastButton.addEventListener('click', choseLeftBreast);
-rightBreastButton.addEventListener('click', choseRightBreast);
-
+leftBreastButton.addEventListener("click", choseLeftBreast);
+rightBreastButton.addEventListener("click", choseRightBreast);
 
 // edit record
 
-const backgroundBlur = document.createElement('div');
+const backgroundBlur = document.createElement("div");
 backgroundBlur.style.cssText = `
   position: fixed;
   top: 0;
@@ -192,29 +216,38 @@ backgroundBlur.style.cssText = `
 `;
 document.body.appendChild(backgroundBlur);
 
-const editRecord = async function(e) {
-const leftBreastButtonEdit = document.createElement('button');
-leftBreastButtonEdit.textContent = 'Lewa';
-const rightBreastButtonEdit = document.createElement('button');
-rightBreastButtonEdit.textContent = 'Prawa';
+const editRecord = async function (e) {
+  const leftBreastButtonEdit = document.createElement("button");
+  leftBreastButtonEdit.textContent = "Lewa";
+  const rightBreastButtonEdit = document.createElement("button");
+  rightBreastButtonEdit.textContent = "Prawa";
 
-if(leftBreastButtonEdit.textContent.toLowerCase() === this.querySelector('p:first-of-type').textContent.toLowerCase()) {
-  leftBreastButtonEdit.classList.add('active');
-} else {
-  rightBreastButtonEdit.classList.add('active');
-}
+  if (
+    leftBreastButtonEdit.textContent.toLowerCase() ===
+    this.querySelector("p:first-of-type").textContent.toLowerCase()
+  ) {
+    leftBreastButtonEdit.classList.add("active");
+  } else {
+    rightBreastButtonEdit.classList.add("active");
+  }
 
-const dateTimeInputEdit = document.createElement('input');
-dateTimeInputEdit.setAttribute('type', 'datetime-local');
-dateTimeInputEdit.value = this.querySelector('p:last-of-type').textContent.replace(' ', 'T');
-const container = document.createElement('div');
-container.classList.add('editRecord');
-container.append(leftBreastButtonEdit, rightBreastButtonEdit, dateTimeInputEdit);
-this.appendChild(container);
-backgroundBlur.style.zIndex = '2';
+  const dateTimeInputEdit = document.createElement("input");
+  dateTimeInputEdit.setAttribute("type", "datetime-local");
+  dateTimeInputEdit.value = this.querySelector(
+    "p:last-of-type"
+  ).textContent.replace(" ", "T");
+  const container = document.createElement("div");
+  container.classList.add("editRecord");
+  container.append(
+    leftBreastButtonEdit,
+    rightBreastButtonEdit,
+    dateTimeInputEdit
+  );
+  this.appendChild(container);
+  backgroundBlur.style.zIndex = "2";
 
-backgroundBlur.addEventListener('click', () => {
-  backgroundBlur.style.zIndex = '-2';
-  container.remove();
-})
+  backgroundBlur.addEventListener("click", () => {
+    backgroundBlur.style.zIndex = "-2";
+    container.remove();
+  });
 };
